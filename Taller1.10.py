2db3281e30afe3f11f571966b4ed4c6b8b69ec8c
jeriosv <142249529+jeriosv@users.noreply.github.com>
2023-09-23 00:56:35 -0500

    Add files via upload

--- Taller1.10.py
+++ Taller1.10.py
@@ -2,7 +2,7 @@ distancia : float
 
 distancia = float(input("Ingrese el valor de una distancia [en metros] : "))
 
-velocidadLuz : float = 299792458      # Velocidad de la Luz [m/s]
+velocidadLuz : float = 299792.458      # Velocidad de la Luz [m/s]
 velocidadSonido : float = 343.2       # Velocidad del sonido (en el aire) [m/s]
 velocidadComercial : float = 141.263889  # Velocidad del vehículo comercial más veloz [m/s]. SSC Tuatara, velocidad máxima reconocida oficialmente de 508,55 km/h
 velocidadBolt : float = 10.4384133612 # Velocidad alcanzada por Usain Bolt en los 100 metros en 9,58s [m/s]
