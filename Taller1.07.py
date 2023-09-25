ed341bbc085702efb8ef95dd66dae4edcb348b11
Jaime Eduardo Ríos Villegas <142249529+jeriosv@users.noreply.github.com>
2023-09-24 19:35:21 -0500

    Add files via upload

--- Taller1.07.py
+++ Taller1.07.py
@@ -1,20 +1,44 @@
-n1 : float 
-n2 : float
-n3 : float
-n4 : float
-n5 : float
+import math
 
-n1 = float(input("Ingrese el primer número real : "))
-n2 = float(input("Ingrese el segundo número real: "))
-n3 = float(input("Ingrese el tercer número real : "))
-n4 = float(input("Ingrese el cuarto número real : "))
-n5 = float(input("Ingrese el quinto número real : "))
+# Pedir 5 números reales
+numeros = []
+for i in range(5):
+    numero = float(input(f"Ingrese el número {i+1}: "))
+    numeros.append(numero)
 
-promedio : float = (n1+n2+n3+n4+n5) / 5
-mediana : float
-promedioMultiplicativo : float = (n1*n2*n3*n4*n5) ** (1/5)
+# Calcular el promedio
+promedio = sum(numeros) / len(numeros)
 
+# Calcular la mediana
+numeros_ordenados = sorted(numeros)
+if len(numeros) % 2 == 0:
+    mediana = (numeros_ordenados[len(numeros) // 2 - 1] + numeros_ordenados[len(numeros) // 2]) / 2
+else:
+    mediana = numeros_ordenados[len(numeros) // 2]
 
-print( " El promedio aritmético de los cinco números es: ", (promedio))
-print( " El promedio multiplicativo de los cinco números es: ", (promedioMultiplicativo))
+# Calcular el promedio multiplicativo
+producto = 1
+for numero in numeros:
+    producto *= numero
+promedio_multiplicativo = math.pow(producto, 1 / len(numeros))
 
+# Ordenar los números de forma ascendente y descendente
+ascendente = sorted(numeros)
+descendente = sorted(numeros, reverse=True)
+
+# Calcular la potencia del mayor número elevado al menor número
+mayor = max(numeros)
+menor = min(numeros)
+potencia = math.pow(mayor, menor)
+
+# Calcular la raíz cúbica del menor número
+raiz_cubica = math.pow(menor, 1/3)
+
+# Mostrar resultados
+print(f"Promedio: {promedio}")
+print(f"Mediana: {mediana}")
+print(f"Promedio Multiplicativo: {promedio_multiplicativo}")
+print(f"Números Ordenados Ascendentemente: {ascendente}")
+print(f"Números Ordenados Descendentemente: {descendente}")
+print(f"Potencia del Mayor al Menor: {potencia}")
+print(f"Raíz Cúbica del Menor: {raiz_cubica}")
